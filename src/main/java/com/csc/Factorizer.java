858d37c8bb23f06f296d0fbede1c7b5b8846e4ef
Ben <b_abdyvaidov@mail.harpercollege.edu>
2024-09-22 18:44:26 -0500

    idk

--- src/main/java/com/csc/Factorizer.java
+++ src/main/java/com/csc/Factorizer.java
@@ -24,7 +24,7 @@ public class Factorizer {
             return "null";
         }
 
-        for (int i = 2; i <= n; i++) { 
+        for (int i = 2; i <= n; i++) {
             while (n % i == 0) {
                 n = n / i;
                 factors.append(i).append(" ");
@@ -38,11 +38,11 @@ public class Factorizer {
     }
 
     public static boolean isPrime(int n) {
-      if (n == 2 || n == 3 || n == 5 || n == 7 || n == 11 || n == 13 || n == 17 || n == 19 || n == 23 ||
-          n == 29 || n == 31 || n == 37 || n == 41 || n == 43 || n == 47 || n == 53 || n == 59 ||
-          n == 61 || n == 67 || n == 71 || n == 73 || n == 79 || n == 83 || n == 89 || n == 97) {
-          return true;
-      }
-      return false;
-  }
+        if (n == 2 || n == 3 || n == 5 || n == 7 || n == 11 || n == 13 || n == 17 || n == 19 || n == 23 ||
+                n == 29 || n == 31 || n == 37 || n == 41 || n == 43 || n == 47 || n == 53 || n == 59 ||
+                n == 61 || n == 67 || n == 71 || n == 73 || n == 79 || n == 83 || n == 89 || n == 97) {
+            return true;
+        }
+        return false;
+    }
 }
